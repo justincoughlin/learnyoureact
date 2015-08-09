b79c29a9b2de91a4a6dc479a1a6553743b697753
Justin Coughlin <software@justincoughlin.com>
2015-08-09 02:37:16 -0700

    props

--- views/index.jsx
+++ views/index.jsx
@@ -15,11 +15,27 @@ var React = require('react');
  var TodoList = React.createClass({
    render: function() {
      return (
-      <div className="todoList">
-        I am a TodoList.
-      </div>
+       <div className = "todoList">
+         <table style={{border: "2px solid black"}}>
+           <tbody>
+             <Todo title="Shopping">Milk</Todo>
+             <Todo title="Hair cut">13:00</Todo>
+           </tbody>
+         </table>
+       </div>
      );
    }
+ });
+
+ var Todo = React.createClass({
+  render: function() {
+  return (
+    <tr>
+      <td style={{border:"1px solid black"}}>{this.props.title}</td>
+      <td style={{border:"1px solid black"}}>{this.props.children}</td>
+    </tr>
+    );
+  }
  });
 
  var TodoForm = React.createClass({
